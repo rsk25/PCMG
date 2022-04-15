f005ab0b0eaec3548e4312153b80aad6c5d73d48
rsk25 <montgomery7289@gmail.com>
2022-04-15 12:49:07 +0000

    exclude questions with '='; change equations so that they have NUM_FORMAT

--- preproc/data.py
+++ preproc/data.py
@@ -1,4 +1,7 @@
 from typing import List, Dict, Union
+import re
+
+NUM_FORMAT = 'N_'
 
 
 class MathWordProblem:
@@ -28,19 +31,27 @@ class MathWordProblem:
     def set_text(self):
         raise NotImplementedError()
 
-    def set_id(self):
-        raise NotImplementedError()
-
-    def set_index(self):
-        raise NotImplementedError()
-
     def set_answer(self):
         raise NotImplementedError()
 
     def set_numbers(self):
         raise NotImplementedError()
 
+    def set_equation(self):
+        raise NotImplementedError()
+
     def as_dict(self):
+        raise NotImplementedError()
+
+
+class MathWordDataset:
+    def __init__(self):
+        pass
+
+    def set_ids(self):
+        raise NotImplementedError()
+
+    def set_indices(self):
         raise NotImplementedError()
 
 
@@ -57,27 +68,35 @@ class Math23kProblem(MathWordProblem):
     def set_text(self):
         self.text = self.oldText
 
-    def set_id(self):
-        # TODO: random number generator
-        self._id = 0
-
-    def set_index(self):
-        # TODO: continue from the last index of PEN?
-        self.index = 0
-
     def set_answer(self):
         self.answers[0].update({"x": int(self.oldAnswer[0])})
 
     def set_numbers(self):
         # TODO: use mwp_template and eps_template
-        self.numbers = self.numbers
+        pass
+
+    def set_equations(self):
+        eqs = self.eqs_template
+        new_eqs = []
+        for eq in eqs:
+            p1 = r'num(\d{2})'
+            p2 = r'num(\d{1})'
+            eq = re.sub(p1, NUM_FORMAT+r'\1', eq)
+            eq = re.sub(p2, NUM_FORMAT+r'0\1', eq)
+            new_eqs.append(eq)
+        self.equations = new_eqs
+
+    def set_exclude(self):
+        exclude_pattern = "="
+        if exclude_pattern in self.text:
+            self._exclude = True
 
     def set_all(self):
         self.set_text()
-        self.set_id()
-        self.set_index()
         self.set_answer()
         self.set_numbers()
+        self.set_equations()
+        self.set_exclude()
         self.is_set = True
 
     def as_dict(self) -> dict:
@@ -94,10 +113,14 @@ class Math23kProblem(MathWordProblem):
             lang=self.lang,
             numbers=self.numbers,
             oldAnswer=self.oldAnswer,
-            oldFormula=self.oldForumla,
+            oldFormula=self.oldFormula,
             oldText=self.oldText,
             text=self.text,
             worker_for_train=self.worker_for_train,
             explanations=self.explanations
         )
     
+class Math23kDataset(MathWordDataset):
+    def __init__(self):
+        super().__init__()
+        pass
