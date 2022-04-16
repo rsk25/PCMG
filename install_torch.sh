fef4842eb79dcc55484e1841dfe8b400e5b51bfd
rsk25 <montgomery7289@gmail.com>
2022-04-16 09:53:23 +0000

    shell script for installing pytorch gpu ver.

---
+++ install_torch.sh
@@ -0,0 +1,3 @@
+pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torchtext==0.9.1
+pip install numpy scipy pandas torch_optimizer transformers
