6d1844c2cb6347c36dfbff4fd2054f6967377417
rsk25 <montgomery7289@gmail.com>
2022-04-18 07:25:11 +0000

    make numbers that follow PEN's standards

--- preproc/data.py
+++ preproc/data.py
@@ -1,10 +1,14 @@
-from os import _EnvironCodeFunc
 import json
 import pickle
 from typing import List, Dict, Union
 import re
+from copy import deepcopy
 
-from common.const.operand import NUM_FORMAT
+from sympy.core import sympify
+from sympy.core.numbers import Integer, Float
+
+from common.const.operand import NUM_PREFIX
+from common.pen.pattern import NUMBER_OR_FRACTION_PATTERN
 
 
 NUMBERS_DEFAULT_FORMAT = {
@@ -16,8 +20,11 @@ NUMBERS_DEFAULT_FORMAT = {
 
 
 
-def to_pen_decimal(fraction_str: str) -> float:
-    pass
+def to_pen_decimal(number_str: str) -> float:
+    assert NUMBER_OR_FRACTION_PATTERN.fullmatch(number_str)
+    number_str = re.sub(',','',number_str)
+    number = sympify(number_str, evaluate=True, rational=False)
+    return number
 
 
 class MathWordProblem:
@@ -83,6 +90,14 @@ class Math23kProblem(MathWordProblem):
         self.eqs_template = eqs_template
         self.is_set = False
 
+
+    def _math23k_to_pen_format(self, math23k_str: str) -> str:
+        pen_pattern1 = r'num(\d{2})'
+        pen_pattern2 = r'num(\d{1})'
+        pen_str = re.sub(pen_pattern1, NUM_PREFIX+r'\1', math23k_str)
+        pen_str = re.sub(pen_pattern2, NUM_PREFIX+r'0\1', pen_str)
+        return pen_str
+
     
     def set_text(self) -> None:
         self.text = self.oldText
@@ -92,35 +107,41 @@ class Math23kProblem(MathWordProblem):
         self.answers[0].update({"x": int(self.oldAnswer[0])})
 
 
-    def set_numbers(self) -> None:
-        # TODO: use mwp_template and eps_template
-        # key, token, tokenRange
-        pair_for_parsing = zip(self.oldText.split(), self.mwp_template.split())
-        p1 = r'^num(\d{1,2})$'
-
-        
-        def _math23k_to_pen_format(math23k_str: str) -> str:
-            pen_pattern1 = r'num(\d{2})'
-            pen_pattern2 = r'num(\d{1})'
-            math23k_str = re.sub(pen_pattern1, NUM_FORMAT+r'\1', math23k_str)
-            pen_str = re.sub(pen_pattern2, NUM_FORMAT+r'0\1', math23k_str)
-            return pen_str
-
-        
-        for i, (orig_text, labeled_text) in enumerate(pair_for_parsing):
-            if re.match(p1, labeled_text):
-                self.numbers.append(NUMBERS_DEFAULT_FORMAT)
-                self.numbers[-1].update({'key': _math23k_to_pen_format(labeled_text)})
-                self.numbers[-1].update({'token': [orig_text]})
-                self.numbers[-1].update({'tokenRange':[i]})
-                self.numbers[-1].update({'value': to_pen_decimal(orig_text)})
-        
-
     def set_equations(self) -> None:
         eqs = self.eqs_template
         new_eqs = [self._math23k_to_pen_format(eq) for eq in eqs]
         self.equations = new_eqs
 
+
+    def set_numbers(self) -> None:
+        
+        def _update_numbers(_number ,key_name: str, update_value: Union[List[Union[str, int]], str]):
+            assert type(update_value) in [list, str, Integer, Float], f"update_value is {type(update_value)}"
+            _number.update({key_name: update_value})
+
+        def delete_whitespace(template: str) -> str:
+            template = re.sub(r"(\S+)\s+([-.,!?')};:]+)", r"\1\2", template)
+            template = re.sub(r"([-.,!?')};:]+)\s+([-.,!?')};:]+)", r"\1\2", template)
+            template = re.sub(r"([-({']+)\s+(\S+)", r"\1\2", template)
+            return template
+        
+        self.mwp_template = delete_whitespace(self.mwp_template)
+        mwp_template_split = self.mwp_template.split()
+        oldText_split = self.oldText.split()
+        assert len(mwp_template_split) == len(oldText_split), \
+            f"MWP template does not match oldText by {abs(len(mwp_template_split) - len(oldText_split))} tokens!\n"
+        pair_for_parsing = zip(oldText_split, mwp_template_split)
+        p1 = r'num(\d{1,2})'
+
+        for i, (orig_text, labeled_text) in enumerate(pair_for_parsing):
+            if re.fullmatch(p1, labeled_text):
+                new_number = deepcopy(NUMBERS_DEFAULT_FORMAT)
+                _update_numbers(new_number, 'key', self._math23k_to_pen_format(labeled_text))
+                _update_numbers(new_number, 'token', [orig_text])
+                _update_numbers(new_number, 'tokenRange', [i])
+                _update_numbers(new_number, 'value', to_pen_decimal(orig_text))
+                self.numbers.append(new_number)
+                
 
     def set_exclude(self) -> None:
         exclude_pattern = "="
