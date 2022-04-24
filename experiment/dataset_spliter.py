f559e9110e7807293f90c2e05bc5ab4d91c2f59a
rsk25 <montgomery7289@gmail.com>
2022-04-24 10:28:52 +0000

    add dataset spliting submodule

---
+++ experiment/dataset_spliter.py
@@ -0,0 +1,15 @@
+from numpy import arange
+from numpy.random import default_rng
+
+random = default_rng(9172)
+
+# get lenght of dataset
+
+# split dataset using numpy into train/dev/test -> ratio: 8:1:1
+    # must not overlap
+    # choose without replacement
+
+# write split to non-extension text files
+    # default save directory: /resource/experiments/pen/
+
+
