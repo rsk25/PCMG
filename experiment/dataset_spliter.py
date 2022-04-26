2fb654819218c3b54157f1519e03c6fc9740ccb0
rsk25 <montgomery7289@gmail.com>
2022-04-26 07:18:59 +0000

    get indices of train/dev/test

--- experiment/dataset_spliter.py
+++ experiment/dataset_spliter.py
@@ -1,8 +1,9 @@
-from typing import List, Dict, Any
+from types import new_class
+from typing import List, Dict, Any, Tuple
 from pathlib import Path
 import json
 
-from numpy import arange
+import numpy as np
 from numpy.random import default_rng
 
 random = default_rng(9172)
@@ -11,10 +12,27 @@ random = default_rng(9172)
 # split dataset using numpy into train/dev/test -> ratio: 8:1:1
     # must not overlap
     # choose without replacement
+def split_train_dev_test_indices(dataset: List[Dict[Any, Any]]) -> Tuple[List[int], List[int], List[int]]:
+    total_num = len(dataset)
+    
+    # get test indices
+    indices = np.arange(total_num)
+    test_indices = random.choice(indices, total_num//10, replace=False).tolist()
+    # get validation indices
+    indices = np.array([i for i in indices if i not in test_indices])
+    dev_indices = random.choice(indices, total_num//10, replace=False).tolist()
+    # get train indices
+    train_indices = [i for i in indices if i not in dev_indices]
+    # sanity check
+    number_of_indices = len(test_indices) + len(dev_indices) + len(train_indices)
+    assert number_of_indices == total_num, \
+        f"total is {total_num}, but the number of indices is {number_of_indices}"
 
+    
+    return train_split, dev_split, test_split
 
 # write split to non-extension text files
     # default save directory: /resource/experiments/pen/
 
 
-__all__ = []
+__all__ = ['split_train_dev_test_indices']
--- split_experiments.py
+++ split_experiments.py
@@ -3,7 +3,10 @@ import json
 
 from experiment.dataset_spliter import *
 
-DATA_PATH = Path('/resource/dataset/new_pen.json')
+
+DATA_PATH = Path('./resource/dataset/new_pen.json')
+EXP_PATH = Path('./experiments/pen/')
+
 
 if __name__ == "__main__":
 
@@ -12,5 +15,21 @@ if __name__ == "__main__":
     
     assert type(dataset) == list
     
-    total_num = len(dataset)
+    train_index, dev_index, test_index = split_train_dev_test_indices(dataset)
+    print(f"train_index: {train_index}")
+    print(f"dev_index: {dev_index}")
+    print(f"test_index: {test_index}")
+    # train_split, dev_split, test_split = split_train_dev_test_id(dataset)
 
+
+    # TRAIN = EXP_PATH / 'train'
+    # with TRAIN.open('w+t') as fp:
+    #     fp.writelines(train_split)
+
+    # DEV = EXP_PATH / 'dev'
+    # with DEV.open('w+t') as fp:
+    #     fp.writelines(dev_split)
+
+    # TEST = EXP_PATH / 'test'
+    # with TEST.open('w+t') as fp:
+    #     fp.writelines(test_split)
