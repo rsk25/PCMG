b3bdf4615a6ba4bd4df4083398918e7e06268718
rsk25 <montgomery7289@gmail.com>
2022-04-26 07:22:22 +0000

    refactor index setting functionality

--- experiment/dataset_spliter.py
+++ experiment/dataset_spliter.py
@@ -12,24 +12,23 @@ random = default_rng(9172)
 # split dataset using numpy into train/dev/test -> ratio: 8:1:1
     # must not overlap
     # choose without replacement
-def split_train_dev_test_indices(dataset: List[Dict[Any, Any]]) -> Tuple[List[int], List[int], List[int]]:
+def set_indices(dataset: List[Dict[Any, Any]]) -> Tuple[List[int], List[int], List[int]]:
     total_num = len(dataset)
     
-    # get test indices
+    # set test indices
     indices = np.arange(total_num)
     test_indices = random.choice(indices, total_num//10, replace=False).tolist()
-    # get validation indices
+    # set validation indices
     indices = np.array([i for i in indices if i not in test_indices])
     dev_indices = random.choice(indices, total_num//10, replace=False).tolist()
-    # get train indices
+    # set train indices
     train_indices = [i for i in indices if i not in dev_indices]
     # sanity check
     number_of_indices = len(test_indices) + len(dev_indices) + len(train_indices)
     assert number_of_indices == total_num, \
-        f"total is {total_num}, but the number of indices is {number_of_indices}"
+        f"total is {total_num}, but the number of indices is {number_of_indices}; they are not the same"
 
-    
-    return train_split, dev_split, test_split
+    return train_indices, dev_indices, test_indices
 
 # write split to non-extension text files
     # default save directory: /resource/experiments/pen/
