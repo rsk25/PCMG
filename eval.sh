721051cad50ee359d4bf438b50110d5416ac48ed
rsk25 <montgomery7289@gmail.com>
2022-05-11 11:08:55 +0000

    script for test running only model testing

--- eval.sh
+++ eval.sh
@@ -6,8 +6,8 @@ MODEL_NAME=./runs/$1
 export TOKENIZERS_PARALLELISM=false
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-DATA=./resource/dataset/pen.json
-SUBSET_TYPE=pen
+DATA=./resource/dataset/new_pen.json
+SUBSET_TYPE=math23k
 EXPERIMENT=./resource/experiments/${SUBSET_TYPE}
 
 python test_model.py -data $DATA -exp $EXPERIMENT -model ${MODEL_NAME}/best_* -ntr 500 -smp 50 -cpu 1 -gpu 0.5 
