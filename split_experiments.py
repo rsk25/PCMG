745b80fb3f4bb9c2db97199b2d52e8521a92327b
rsk25 <montgomery7289@gmail.com>
2022-05-11 11:00:16 +0000

    debug splitting method

--- split_experiments.py
+++ split_experiments.py
@@ -1,18 +1,24 @@
 from pathlib import Path
 import json
 import random
+import argparse
 
 from experiment.dataset_spliter import *
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--data-path', '-d')
+parser.add_argument('--experiment-path', '-e')
 
-DATA_PATH = Path('./resource/dataset/new_pen.json')
-EXP_PATH = Path('./resource/experiments/pen/')
-
+# DATA_PATH = Path('./resource/dataset/new_pen.json')
+# EXP_PATH = Path('./resource/experiments/new_pen/')
 
 if __name__ == "__main__":
 
+    args = parser.parse_args()
+
     random.seed(9172)
 
+    DATA_PATH = Path(f"./resource/dataset/{args.data_path}")
     with DATA_PATH.open('r+t') as fp:
         dataset = json.load(fp)
     
@@ -21,6 +27,7 @@ if __name__ == "__main__":
     id_list = get_ids(dataset)
     train_split, dev_split, test_split = split_data(id_list)
 
+    EXP_PATH = Path(f"./resource/experiments/{args.experiment_path}")
     TRAIN = EXP_PATH / 'train'
     with TRAIN.open('w+t') as fp:
         for i in train_split:
