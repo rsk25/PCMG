fbe946d1b8c19bccbf8fad65362583b465f97c51
rsk25 <montgomery7289@gmail.com>
2022-06-21 01:55:14 +0000

    change argument from bool type to str type

--- train_model.py
+++ train_model.py
@@ -46,7 +46,7 @@ def read_arguments():
 
     log = parser.add_argument_group('Logger setup')
     log.add_argument('--log-path', '-log', type=str, default='./runs')
-    log.add_argument('--detect-anomaly', action='store_true')
+    log.add_argument('--detect-anomaly', '-da', choices=['true','false'], default='false')
 
     work = parser.add_argument_group('Worker setup')
     work.add_argument('--gpu-ids', '-ids', type=str, default=GPU_IDS)
